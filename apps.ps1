f23f38fc5dc701ef95e719e1ddc51d9af5bfa250
osvdi <141240199+osvdi@users.noreply.github.com>
2023-08-22 09:21:43 -0400

    Update apps.ps1

--- apps.ps1
+++ apps.ps1
@@ -1,5 +1,6 @@
+choco install -y putty.install
 choco install -y notepadplusplus
 choco install -y winscp
 choco install -y javaruntime
 choco install -y greenshot
-choco install -y 7zip
+choco install -y googlechrome
